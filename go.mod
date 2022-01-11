500bd6e2a89c88102651f3050039300fad4ed45e
christinegotth <christine.gotth@gmail.com>
2022-01-11 21:01:31 +0100

    Add stroma metrics

--- go.mod
+++ go.mod
@@ -12,7 +12,11 @@ require (
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/pion/datachannel v1.4.21 // indirect
 	github.com/pion/dtls/v2 v2.0.10 // indirect
 	github.com/pion/ice/v2 v2.1.12 // indirect
@@ -29,8 +33,13 @@ require (
 	github.com/pion/transport v0.12.3 // indirect
 	github.com/pion/turn/v2 v2.0.5 // indirect
 	github.com/pion/udp v0.1.1 // indirect
+	github.com/prometheus/client_golang v1.11.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.26.0 // indirect
+	github.com/prometheus/procfs v0.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/net v0.0.0-20211005001312-d4b1ae081e3b // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/protobuf v1.26.0-rc.1 // indirect
 )
--- main.go
+++ main.go
@@ -16,8 +16,18 @@ import (
 	"github.com/pion/webrtc/v3"
 	"github.com/pion/webrtc/v3/pkg/media"
 	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
 	"gopkg.in/hraban/opus.v2"
+)
+
+var (
+	currentClients = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "streama_current_clients",
+		Help: "The current number of viewers",
+	})
 )
 
 func main() {
@@ -36,6 +46,7 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/sdp", handleClient(log, addClient))
+	mux.Handle("/metrics", promhttp.Handler())
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: cors.AllowAll().Handler(mux),
@@ -225,10 +236,13 @@ func initConn(log logrus.FieldLogger, rtcConn *webrtc.PeerConnection, clientConn
 		case webrtc.PeerConnectionStateConnected:
 			state.Set(ConnectionStateConnected)
 			clientConnected <- conn
+			currentClients.Inc()
 		case webrtc.PeerConnectionStateDisconnected:
 			state.Set(ConnectionStateDisconnected)
+			currentClients.Dec()
 		case webrtc.PeerConnectionStateClosed, webrtc.PeerConnectionStateFailed:
 			state.Set(ConnectionStateClosed)
+			currentClients.Dec()
 		}
 	})
 
